c6b1dd677c13d43cdfa130bc5e0ba086dfd8547d
muhamadpandudwiputra <112594630+muhamadpandudwiputra@users.noreply.github.com>
2023-04-12 12:30:34 +0700

    Add files via upload

---
+++ program membaca input dari keyword.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main (){
+	
+int a;
+long int b;
+char c;
+float d;
+double e;
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;
+cin>>e;
+
+cout<<a<<endl;
+cout<<b<<endl;
+cout<<c<<endl;
+cout<<setprecision(3)<<fixed<<d<<endl;
+cout<<setprecision(9)<<fixed<<e<<endl;
+
+return 0;
